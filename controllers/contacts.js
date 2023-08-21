c97416b92a9f3bc12949244c17a5414f19fc852c
Tania Petruk <127411602+tanightt@users.noreply.github.com>
2023-08-21 21:01:17 +0300

    CREATED: controllers, decorators

---
+++ controllers/contacts.js
@@ -0,0 +1,52 @@
+const contacts = require("../models/contacts");
+
+const { HttpError } = require("../helpers");
+
+const ctrlWrapper = require("../decorators/ctrlWrapper");
+
+const getAll = async (req, res) => {
+  const result = await contacts.listContacts();
+  res.status(200).json(result);
+};
+
+const getById = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
+};
+
+const removeContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json({
+    message: "contact deleted",
+  });
+};
+
+const updateContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.status(200).json(result);
+};
+
+module.exports = {
+  getAll: ctrlWrapper(getAll),
+  getById: ctrlWrapper(getById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+};
